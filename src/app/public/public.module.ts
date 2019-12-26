adf6aa5fcf53498e275f24b518c7b7b82515e286
Angel Odiel Trevino Villanueva <angel.trevinov@gmail.com>
2019-12-26 13:29:19 -0600

    Finishing style for projects

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { MainComponent } from './main/main.component';
 import {PublicRoutingModule} from './public-routing.module';
 import { CoverComponent } from './main/cover/cover.component';
+import { ProjectCardComponent } from './main/project-card/project-card.component';
 
 @NgModule({
   declarations: [
     MainComponent,
     CoverComponent,
+    ProjectCardComponent,
   ],
   imports: [
     CommonModule,

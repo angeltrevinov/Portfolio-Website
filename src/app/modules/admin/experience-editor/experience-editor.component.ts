3d4050b51b9d37afa3605ab99ba318a67b8ae1a0
Angel Treviño <angel.trevinov@gmail.com>
2020-01-18 23:18:33 -0600

    Starting experience editor

--- src/app/modules/admin/experience-editor/experience-editor.component.ts
+++ src/app/modules/admin/experience-editor/experience-editor.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
+// =============== WORK EXPERIENCE EDITOR ==================
+/*
+* Web page with the form to edit or register a work experience
+* */
 @Component({
   selector: 'app-experience-editor',
   templateUrl: './experience-editor.component.html',
@@ -7,9 +12,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperienceEditorComponent implements OnInit {
 
-  constructor() { }
+  experienceForm: FormGroup;
 
+  // -------------------------------------------------------
+  constructor() { }
+  // TODO: ANGULAR METHODS
+  // -------------------------------------------------------
   ngOnInit() {
+    this.createForm();
   }
+  // TODO: FORM METHODS
+  // -------------------------------------------------------
+  createForm() {
+    this.experienceForm = new FormGroup({
+      strCompanyName: new FormControl(null, {
+        validators: [
+          Validators.required
+        ]
+      }),
+      strPosition: new FormControl(null, {
+        validators: [
+          Validators.required
+        ]
+      }),
+      strDesc: new FormControl(null, {
+        validators: [
+          Validators.required
+        ]
+      }),
+      boolWorkingNow: new FormControl(null, {
+        validators: [
+          Validators.required
+        ]
+      }),
+      startDate: new FormControl(null, {
+        validators: [
+          Validators.required,
+        ]
+      }),
+      endDate: new FormControl(null)
+    });
+  }
+  // -------------------------------------------------------
+  get strCompanyName() { return this.experienceForm.get('strCompanyName'); }
+  // -------------------------------------------------------
+  get strPosition() { return this.experienceForm.get('strPosition'); }
+  // -------------------------------------------------------
+  get strDesc() { return this.experienceForm.get('strDesc'); }
+  // -------------------------------------------------------
+  get boolWorkingNow() { return this.experienceForm.get('boolWorkingNow'); }
+  // -------------------------------------------------------
+  get startDate() { return this.experienceForm.get('startDate'); }
+  // -------------------------------------------------------
+  get endDate() { return this.experienceForm.get('endDate'); }
 
 }
